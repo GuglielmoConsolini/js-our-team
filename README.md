e2f91f34b3ab170c197ee27f7daae8b7fad0d492
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-05-06 17:11:40 +0200

    JS My Team M 1,2,3 + bonus

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 Milestone 0 "Creare l’array di oggetti con le informazioni fornite (in allegato zip)."
 
 1 Dichiaro il mio array a cui inserirò gli oggetti attraverso la sintassi con le {}.
+
+
+MILESTONE 1 "Stampare su console, per ogni membro del team: il nome e il ruolo."
+
+1 Attraverso un cilo mi vado ad generare gli elementi del mio array
+2 faccio un console log delle proprietà per ogni oggetto.
+
+
+Milestone 2 "Stampare in pagina, per ogni membro del team: nome, ruolo e immagine, come semplici stringhe.
+Svolgere questa milestone grazie a una funzione può semplificarvi il lavoro per i bonus. La funzione riceve un oggetto e restituisce una stringa di HTML
+
+
+1 In ottica bonus mi scrivo la mia funzione , la quale mi darà attraverso "return" le righe di codice in html che mi servono
+
+2 Inserisco questa funzione dentro il ciclo definendo le variabili del caso
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+let contenitore = document.getElementById("box")
 // Array di oggetti
 let myTeam = [
 { name:"Wayne Barnett" , ruolo:"Founder & CEO" , foto:"wayne-barnett-founder-ceo.jpg"},
@@ -6,11 +6,31 @@ let myTeam = [
 { name: "Walter Gordon" , ruolo:"Office Manager" , foto:"walter-gordon-office-manager.jpg"},
 { name: "Angela Lopez" , ruolo:"Social Media Manager" , foto:"angela-lopez-social-media-manager.jpg"},
 { name: "Scott Estrada" , ruolo:"Developer" , foto:"scott-estrada-developer.jpg"},
-{ name: "Barbara Ramos" , ruolo:"Graphic Designer" , foto:"barbara-ramos-graphic.jpg"}
+{ name: "Barbara Ramos" , ruolo:"Graphic Designer" , foto:"barbara-ramos-graphic-designer.jpg"}
 ];
-
 console.log(myTeam)
 
+// ciclo per stampare in console le proprietà degli oggetti dentro l'array
+for(let i = 0 ; i < myTeam.length ; i++){
+
+   const membroTeam = myTeam[i];
+   const membroHtml = creaMembroTeamHTML(myTeam[i])
+   console.log("Stampo l'oggetto: " + membroTeam[i])
+   console.log("Stampo il nome e il ruolo: " + membroTeam.name + " ," + membroTeam.ruolo)
+   contenitore.innerHTML += membroHtml;
+
+}
+
+// Definisci la funzione che restituirà una stringa di HTML per un membro del team
+function creaMembroTeamHTML(member) {
+    return `
+        <div class="team-member">
+            <p>Nome: ${member.name}</p>
+            <p>Ruolo: ${member.ruolo}</p>
+            <img src="./img/${member.foto}" alt="${member.name}">
+        </div>
+    `;
+}
 
 
 
